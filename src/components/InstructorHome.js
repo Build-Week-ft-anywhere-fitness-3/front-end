2567198572a3d153e247c7590019969e2c68b0fc
Yuriko Takamiya <yurikotakamiya@YurikonoMacBook-Pro-2.local>
2022-02-01 18:08:47 -0500

    added some styling

--- src/components/InstructorHome.js
+++ src/components/InstructorHome.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ClassList from './ClassList';
+import styled from 'styled-components';
+
 
 const InstructorHome = (props) => {
     
     return (
-        <div className="instructor-home">            
-            <h1>Hi, {props.username}</h1>
-            <h2>Class Lists</h2>
-            <ClassList />
-        </div>
+        <ComponentContainer className="instructor-home">  
+            <ModalContainer>
+                <h1>Hi, {props.username}</h1>
+                <h2>Class Lists</h2>
+                <ClassList />
+            </ModalContainer>          
+        </ComponentContainer>
     )
 }
 export default InstructorHome;
+
+const ComponentContainer = styled.div`
+    height: 70%;
+    justify-content: center;
+    align-items: center;
+    display:flex;
+`
+const ModalContainer = styled.div`
+    width: 500px;
+    background: white;
+    padding: 2rem;
+    text-align: center;
+`
